479a968a85bcb071bc906d1150f5d1e5c294b5d5
ivan <sinitsyn@awok.ae>
2019-01-27 13:30:25 +0400

    Code review. Snippet source documented. Simple for loops changed for list comprehensions. Redundant functions removed.

--- instagram_giveaway.py
+++ instagram_giveaway.py
@@ -10,22 +10,20 @@ from instabot import Bot
 bot = Bot()
 
 
-def get_user_id(username):
-    user_id = bot.get_user_id_from_username(username.lstrip('@'))
-    return user_id
-
-
 def confirm_mentioned_users_are_exists(mentioned_usernames):
-    users_exists = []
-    for username in mentioned_usernames:
-        user_id = get_user_id(username)
-        if user_id is None:
-            continue
-        users_exists.append(user_id)
+    users_exists = [
+        bot.get_user_id_from_username(username)
+        for username in mentioned_usernames
+    ]
+    users_exists = list(filter(lambda x: x, users_exists))
     return users_exists
 
 
 def get_usernames_mentioned_in_comment(comment_text):
+    """
+    RegEx pattern taken from
+    https://blog.jstassen.com/2016/03/code-regex-for-instagram-username-and-hashtags/
+    """
     pattern = '(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]' \
               '|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)'
     return re.findall(pattern, comment_text)
@@ -50,21 +48,11 @@ def comment_satisfies_conditions(comment, post_likers, author_followers):
     followers_mentioned_users_intersection = \
         set(users_mentioned_in_comment).intersection(set(user_followers))
 
-    if not followers_mentioned_users_intersection:
-        return False
-
-    if str(comment_author_user_id) not in post_likers:
-        return False
-
-    if str(comment_author_user_id) not in author_followers:
-        return False
-
-    return True
-
-
-def get_post_likers(post_id):
-    post_likers = bot.get_media_likers(post_id)
-    return post_likers
+    return all([
+        followers_mentioned_users_intersection,
+        str(comment_author_user_id) not in post_likers,
+        str(comment_author_user_id) not in author_followers
+    ])
 
 
 def get_author_followers(author_username):
@@ -74,7 +62,7 @@ def get_author_followers(author_username):
 
 def get_all_allowed_competitors(author_username, post_id):
     post_comments = bot.get_media_comments_all(post_id)
-    post_likers = get_post_likers(post_id)
+    post_likers = bot.get_media_likers(post_id)
     author_followers = get_author_followers(author_username)
 
     competitors = []
@@ -92,14 +80,7 @@ def get_all_allowed_competitors(author_username, post_id):
     return competitors
 
 
-def get_post_id(post_url):
-    post_id = bot.get_media_id_from_link(post_url)
-    return post_id
-
-
 def authenticate_with_bot(instagram_login, instagram_password):
-    load_dotenv()
-
     loggged_in_succesfully = bot.login(
         username=instagram_login,
         password=instagram_password,
@@ -120,6 +101,7 @@ def create_argument_parser():
 
 
 if __name__ == '__main__':
+    load_dotenv()
 
     argument_parser = create_argument_parser()
     args = argument_parser.parse_args()
@@ -134,7 +116,7 @@ if __name__ == '__main__':
         Check instagram_login and instagram_password env variables.
         ''')
 
-    post_id = get_post_id(post_url)
+    post_id = bot.get_media_id_from_link(post_url)
     if not post_id:
         sys.exit('''
         Wrong instagram post url
